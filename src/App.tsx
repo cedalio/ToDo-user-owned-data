d401def0f3b51a174f522ca53d2d82bfb9a1c44d
Jeronimo Clinaz <jeronimo.clinaz@cedalio.com>
2023-03-20 17:10:19 -0300

    First look at privyh over WC

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,7 @@
 import React, { useEffect } from "react";
 import "./App.css"
-import {
-  EthereumClient,
-  modalConnectors,
-  walletConnectProvider,
-} from "@web3modal/ethereum";
-import { Web3Modal } from "@web3modal/react";
-import { configureChains, createClient, WagmiConfig, useAccount } from "wagmi";
-import { polygonMumbai } from "wagmi/chains";
-import { Web3Button } from "@web3modal/react";
+import { usePrivy } from '@privy-io/react-auth';
+
 import axios from "axios";
 
 import Header from "./components/Header";
@@ -40,34 +33,35 @@ const style = {
   flexDirection: "column",
 };
 
-const chains = [polygonMumbai];
-
-const projectId = String(process.env.REACT_APP_WC_PROJECT_ID)
 const TRACKING_ID = String(process.env.REACT_APP_TRACKING_ID)
+// const chains = [polygonMumbai];
 
-// Wagmi client
-const { provider } = configureChains(chains, [
-  walletConnectProvider({ projectId: projectId }),
-]);
-const wagmiClient = createClient({
-  autoConnect: true,
-  connectors: modalConnectors({ appName: "web3Modal", chains }),
-  provider,
-});
+// const projectId = String(process.env.REACT_APP_WC_PROJECT_ID)
 
-// Web3Modal Ethereum Client
-const ethereumClient = new EthereumClient(wagmiClient, chains);
+// // Wagmi client
+// const { provider } = configureChains(chains, [
+//   walletConnectProvider({ projectId: projectId }),
+// ]);
+// const wagmiClient = createClient({
+//   autoConnect: true,
+//   connectors: modalConnectors({ appName: "web3Modal", chains }),
+//   provider,
+// });
+
+// // Web3Modal Ethereum Client
+// const ethereumClient = new EthereumClient(wagmiClient, chains);
 
 export default function App() {
   const [deployed, setDeployed] = React.useState(false);
   const [uri, setUri] = React.useState('');
   const [contractAddress, setContractAddress] = React.useState<string | undefined>();
   const projectId = String(process.env.REACT_APP_WC_PROJECT_ID)
-  const { address } = useAccount()
+  // const { address } = useAccount()
   const [open, setOpen] = React.useState(false);
   const [response, setResponse] = React.useState("");
   const handleClose = () => setOpen(false);
-  
+  const { ready, authenticated, user, login } = usePrivy()
+
   function requestDeployToGateway(address: string) {
     const url = `${process.env.REACT_APP_GRAPHQL_GATEWAY_BASE_URL}/deploy`
     const payload = {
@@ -105,27 +99,32 @@ export default function App() {
       })
   }
 
-  function redeploy(){
+  function redeploy() {
     setResponse("")
-    return requestDeployToGateway(String(address))
+    if (user?.wallet?.address) {
+      return requestDeployToGateway(String(user?.wallet?.address))
+    }
   }
 
   useEffect(() => {
     const deployed = Boolean(localStorage.getItem('deployed'))
     const contractAddress = localStorage.getItem('contractAddress')
     const deploymentId = localStorage.getItem('deploymentId')
+
     if (deployed && contractAddress && deploymentId) {
       setUri(`${String(process.env.REACT_APP_GRAPHQL_GATEWAY_BASE_URL)}/${deploymentId}/graphql`)
       setDeployed(deployed)
       setContractAddress(contractAddress)
     }
-    else if (address) {
-      requestDeployToGateway(address)
+    else if (ready && authenticated) {
+      if (user?.wallet?.address) {
+        return requestDeployToGateway(String(user?.wallet?.address))
+      }
     }
     else {
       return
     }
-  }, [address])
+  }, [ready, authenticated])
 
   const client = new ApolloClient({
     uri,
@@ -137,76 +136,71 @@ export default function App() {
   ReactGA.initialize(TRACKING_ID);
 
   const Loader = () => {
-    if(response === "error"){
+    if (response === "error") {
       return (
-          <div className="loader-layer" style={{justifyContent: 'center', display:"flex", flexDirection:"column"}}>
-              <p className='error-message' style={{textTransform:'uppercase', textAlign:"center", color:"black", fontWeight:"200"}}>We had a <strong> problem </strong>trying to deploy please <strong>retry</strong>.</p>
-              <button className="retry-button" onClick={redeploy}>RETRY</button>
-          </div>
+        <div className="loader-layer" style={{ justifyContent: 'center', display: "flex", flexDirection: "column" }}>
+          <p className='error-message' style={{ textTransform: 'uppercase', textAlign: "center", color: "black", fontWeight: "200" }}>We had a <strong> problem </strong>trying to deploy please <strong>retry</strong>.</p>
+          <button className="retry-button" onClick={redeploy}>RETRY</button>
+        </div>
       )
     }
-    else{
+    else {
       return (
-          <div className="loader-layer" style={{justifyContent: 'center'}}>
-              <Rings
-                  height="100"
-                  width="100"
-                  radius={2}
-                  color="#FF7D3A"
-                  ariaLabel="puff-loading"
-                  wrapperStyle={{}}
-                  wrapperClass=""
-                  visible={true}
-              />
-          </div>
+        <div className="loader-layer" style={{ justifyContent: 'center' }}>
+          <Rings
+            height="100"
+            width="100"
+            radius={2}
+            color="#FF7D3A"
+            ariaLabel="puff-loading"
+            wrapperStyle={{}}
+            wrapperClass=""
+            visible={true}
+          />
+        </div>
       )
     }
-        
-    }
+
+  }
 
   return (
     <ApolloProvider client={client}>
       <div className="App">
-        <WagmiConfig client={wagmiClient}>
-          <Header />
-          <div className={address ? `button-container connected` : `button-container`} >
-            <Web3Button />
+        <Header />
+        <div className={authenticated ? `button-container connected` : `button-container`} >
+          <button onClick={login} >LOGIN TO DEPLOY</button>
+        </div>
+        <div className="gif-container" style={authenticated ? { display: 'none' } : { display: 'flex' }} >
+          <div className="web">
+            <img className="gif" src="home-gif.gif" alt="explained gif" />
           </div>
-          <div className="gif-container" style={address ? { display: 'none' } : { display: 'flex' }} >
-            <div className="web">
-              <img className="gif" src="home-gif.gif" alt="explained gif" />
-            </div>
-          </div>
-          {deployed ? <ListComponent address={address} /> : null}
-          <Web3Modal
-            projectId={projectId}
-            ethereumClient={ethereumClient}
-          />
-          <Modal
-            aria-labelledby="transition-modal-title"
-            aria-describedby="transition-modal-description"
-            open={open}
-            onClose={handleClose}
-            closeAfterTransition
-            BackdropComponent={Backdrop}
-            BackdropProps={{
-              timeout: 500,
-            }}
-          >
-            <Fade in={open}>
-              <Box sx={style}>
-                <Typography id="transition-modal-title" variant="h6" component="h2" sx={{ fontWeight: "800", textAlign: "center" }}>
-                  Creating your account and deploying the Smart Contract Database in Polygon Mumbai!
-                </Typography>
-                <Typography id="transition-modal-description" sx={{ mt: 2, textAlign: "center" }}>
-                  This could take between <strong> 15 to 30 seconds</strong> depending on the network congestion. Please <strong>don’t close the window.</strong>
-                </Typography>
-                <Loader/>
-              </Box>
-            </Fade>
-          </Modal>
-          <Footer contractAddress={contractAddress} />
-        </WagmiConfig>
+        </div>
+        {deployed ? <ListComponent address={user?.wallet?.address} /> : null}
+        <Modal
+          aria-labelledby="transition-modal-title"
+          aria-describedby="transition-modal-description"
+          open={open}
+          onClose={handleClose}
+          closeAfterTransition
+          BackdropComponent={Backdrop}
+          BackdropProps={{
+            timeout: 500,
+          }}
+        >
+          <Fade in={open}>
+            <Box sx={style}>
+              <Typography id="transition-modal-title" variant="h6" component="h2" sx={{ fontWeight: "800", textAlign: "center" }}>
+                Creating your account and deploying the Smart Contract Database in Polygon Mumbai!
+              </Typography>
+              <Typography id="transition-modal-description" sx={{ mt: 2, textAlign: "center" }}>
+                This could take between <strong> 15 to 30 seconds</strong> depending on the network congestion. Please <strong>don’t close the window.</strong>
+              </Typography>
+              <Loader />
+            </Box>
+          </Fade>
+        </Modal>
+        <Footer contractAddress={contractAddress} />
+        {/* </WagmiConfig> */}
       </div>
     </ApolloProvider>
   );
--- src/index.tsx
+++ src/index.tsx
@@ -5,6 +5,7 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
 import { HttpLink } from '@apollo/client';
+import { PrivyProvider } from '@privy-io/react-auth';
 
 const link = new HttpLink({
   uri: String(process.env.REACT_APP_GRAPHQL_URL),
@@ -19,14 +20,19 @@ const client = new ApolloClient({
     addTypename: false,
   }),
 });
-
+const privyAppId = String(process.env.REACT_APP_PRIVY_APP_ID)
+const handleLogin = async (e: any) => {
+  console.log(e)
+}
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
+  <PrivyProvider appId={privyAppId} onSuccess={handleLogin}>
     <ApolloProvider client={client}>
       <App />
-    </ApolloProvider>,
+    </ApolloProvider>
+  </PrivyProvider>
 );
 
 // If you want to start measuring performance in your app, pass a function
